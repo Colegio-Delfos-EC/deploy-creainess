668d89056f2829bd793d77871929afe1bfc3f912
Diegx22v <bydiego500@gmail.com>
2024-12-24 13:22:04 -0500

    cambios en archivo y actualizacion

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = 'TECHCON.zip';
-    link.download = 'TECHCON.zip'; 
+    link.href = 'Creainess.zip';
+    link.download = 'Creainess.zip'; 
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
